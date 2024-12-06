65212513a1ac5f49ee76b9454a63706aaa38d398
Joshua Soberg <soberg.dev@pm.me>
2024-12-06 13:46:29 -0500

    Add initial setup files for utility repo

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "Kotlin AoC Utilities"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
