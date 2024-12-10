933045081fe312d687edebf1937efa557132f285
Joshua Soberg <soberg.dev@pm.me>
2024-12-10 14:23:46 -0500

    Updating to latest version (2024.5)

--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -1,5 +1,5 @@
 object Publishing {
     const val ArtifactId = "kotlin-aoc-utilities"
     const val GroupId = "com.github.jsoberg"
-    const val Version = "2024.4"
+    const val Version = "2024.5"
 }
