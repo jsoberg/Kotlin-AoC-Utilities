c2d011b93cf33fa11503c32f050a9a54860434cd
Joshua Soberg <soberg.dev@pm.me>
2025-12-06 10:32:41 -0500

    Rename artifact for easier maven local usage

--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -1,5 +1,5 @@
 object Publishing {
-    const val ArtifactId = "kotlin-aoc-utilities"
+    const val ArtifactId = "Kotlin-AoC-Utilities"
     const val GroupId = "com.github.jsoberg"
     const val Version = "2025.2"
 }
